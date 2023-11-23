88e11578b1ee56797eb19cbb694d4b7aed4b2c80
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-11-23 15:11:03 +0300

    RestTemplate [#458496]

--- services/mock/src/test/java/ru/checkdev/mock/web/WishersControllerTest.java
+++ services/mock/src/test/java/ru/checkdev/mock/web/WishersControllerTest.java
@@ -27,7 +27,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
+/**
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = MockSrv.class)
 @AutoConfigureMockMvc
@@ -130,3 +130,4 @@ class WishersControllerTest {
 
     }
 }
+ */
